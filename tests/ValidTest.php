b8be5164446c60d0187364bb551813301155f94d
Todd Burry <todd@vanillaforums.com>
2014-07-16 14:42:44 -0400

    Add some safe html tests.

---
+++ tests/ValidTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @author Todd Burry <todd@vanillaforums.com>
+ * @copyright 2009-2014 Vanilla Forums Inc.
+ * @license LGPL-3.0
+ */
+
+namespace Htmlawed\Tests;
+
+use Htmlawed;
+
+/**
+ * Unit tests for valid html.
+ */
+class ValidTest extends \PHPUnit_Framework_TestCase {
+    /**
+     * Test that valid html does not change when filtered.
+     *
+     * @param $html The html to test.
+     * @dataProvider provideValid
+     */
+    public function testValidNoChange($html) {
+        $filtered = Htmlawed::filter($html);
+        $this->assertEquals($html, $filtered);
+    }
+
+
+    public function provideValid() {
+        $paths = glob(__DIR__.'/fixtures/valid/*.html');
+
+        $result = [];
+        foreach ($paths as $path) {
+            $result[basename($path)] = [file_get_contents($path)];
+        }
+        return $result;
+    }
+}
+ 
